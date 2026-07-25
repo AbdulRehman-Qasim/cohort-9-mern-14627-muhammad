8280673e2aecbda8e6fc8f28e78538b991e0cd9f
Muhammad-Abdul-Rehman1318 <fa22-bse-074@cuilahore.edu.pk>
2026-07-25 20:35:13 +0500

    chore: project structure cleanup and config additions

---
+++ backend/eslint.config.js
@@ -0,0 +1,21 @@
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
